9c7b01591dfa237521c252aba6d7faf19373682e
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-11-24 12:45:05 -0500

    Add imports, check previous labs for thoroughness in doing final

--- Final/Final-4840.py
+++ Final/Final-4840.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
 import numpy as np
 import os
 
